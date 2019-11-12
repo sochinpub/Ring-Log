ec6a517164afb578725c6ef7d739f8e861bc947a
Sochin Jiang <sochin@aliyun.com>
2019-11-13 00:58:22 +0800

    add debug-info

--- example/single-thread-example.cc
+++ example/single-thread-example.cc
@@ -6,6 +6,7 @@ int main(int argc, char** argv)
     LOG_INIT("log", "myname", 3);
     for (int i = 0;i < 1e3; ++i)
     {
+        LOG_WARN("my number is number my number is my number is my number is my number is my number is my number is %d", i);
         LOG_ERROR("my number is number my number is my number is my number is my number is my number is my number is %d", i);
     }
 }
